2a3251222c1d3321c6b5c9cbef433208b1cae05a
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-23 00:08:49 +0300

    add-first-task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,22 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
+const createMarkup = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join("");
+
+list.insertAdjacentHTML("afterbegin", createMarkup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
