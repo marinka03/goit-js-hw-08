458b9c7d3e3f29e235034535fc0baa63c69854c9
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-23 12:51:43 +0300

    add-second-task-about-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+
+// function onPlay(evt) {
+//     localStorage.setItem("videoplayer-current-time", JSON.stringify(evt.seconds));
+// };
+player.on("timeupdate", 
+throttle(function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds))}, 1000));
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+const parsedCurrentTime = JSON.parse(currentTime);
+
+player.setCurrentTime(parsedCurrentTime).then(function(seconds) {
+}).catch(function(error) {
+    console.log(Error);
+});
