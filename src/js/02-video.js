72141ace3073a23c997542c5d9d9fa5ceb652375
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-23 12:54:11 +0300

    add-fix-second-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,9 @@
 import Vimeo from "@vimeo/player";
 import throttle from "lodash.throttle";
 
-
-
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-
-// function onPlay(evt) {
-//     localStorage.setItem("videoplayer-current-time", JSON.stringify(evt.seconds));
-// };
 player.on("timeupdate", 
 throttle(function(data) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds))}, 1000));
